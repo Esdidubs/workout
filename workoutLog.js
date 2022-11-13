1bc9d9a1302bf0acaaddc95e6ea05d8cb8dc9310
Steven Ward <sward3644@gmail.com>
2022-11-13 13:16:16 -0700

    Logs

--- workoutLog.js
+++ workoutLog.js
@@ -1,5 +1,7 @@
 // walk, pull, push, fight
 
 let workoutLog = [
-    { date: "11/12/2022", summary: "morning after a coooold walk", workoutQuantity: 100, workoutType: "push"}
+    { date: "11/12/2022", summary: "morning after a coooold walk", workoutQuantity: 100, workoutType: "push"},
+    { date: "11/12/2022", summary: "walking throughout the day and a coooold walk", workoutQuantity: 3.55, workoutType: "walk"},
+    { date: "11/13/2022", summary: "pullups after lunch", workoutQuantity: 66, workoutType: "pull"}
 ];
