733e7e51b01ddaba23b008c26b8a20363b38ec59
Steven Ward <sward3644@gmail.com>
2022-11-19 14:46:44 -0700

    Logged

--- workoutLog.js
+++ workoutLog.js
@@ -9,5 +9,9 @@ let workoutLog = [
     { date: "11/15/2022", summary: "pushups", workoutQuantity: 100, workoutType: "push"},
     { date: "11/15/2022", summary: "walking", workoutQuantity: 2.96, workoutType: "walk"},
     { date: "11/16/2022", summary: "walking", workoutQuantity: 1.88, workoutType: "walk"},
-    { date: "11/13/2022", summary: "pullups in morning with stiff neck", workoutQuantity: 20, workoutType: "pull"},
+    { date: "11/16/2022", summary: "pullups in morning with stiff neck", workoutQuantity: 20, workoutType: "pull"},
+    { date: "11/17/2022", summary: "walking, pickleball", workoutQuantity: 3.61, workoutType: "walk"},
+    { date: "11/18/2022", summary: "walking, pickleball", workoutQuantity: 3.29, workoutType: "walk"},
+    { date: "11/19/2022", summary: "chest is uneven after", workoutQuantity: 175, workoutType: "push"},
+    { date: "11/19/2022", summary: "fighting after Beat Saber", workoutQuantity: 3, workoutType: "fight"},
 ];
