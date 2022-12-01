58c6462d9436eaf4a654471c188d4350b0ccb832
Steven Ward <sward3644@gmail.com>
2022-12-01 07:17:51 -0700

    Logs

--- workoutLog.js
+++ workoutLog.js
@@ -1,39 +1,42 @@
 // walk, pull, push, fight
 
 let workoutLog = [
-    // WALKING, RUNNING, SWIMMING
-    { date: "11/12/2022", workoutQuantity: 3.55, workoutType: "walk"},
-    { date: "11/13/2022", workoutQuantity: 4.17, workoutType: "walk"},
-    { date: "11/14/2022", workoutQuantity: 4.09, workoutType: "walk"},
-    { date: "11/15/2022", workoutQuantity: 2.96, workoutType: "walk"},
-    { date: "11/16/2022", workoutQuantity: 1.88, workoutType: "walk"},
-    { date: "11/17/2022", workoutQuantity: 3.61, workoutType: "walk"},
-    { date: "11/18/2022", workoutQuantity: 3.29, workoutType: "walk"},
-    { date: "11/19/2022", workoutQuantity: 4.29, workoutType: "walk"},
-    { date: "11/20/2022", workoutQuantity: 3.45, workoutType: "walk"},
-    { date: "11/21/2022", workoutQuantity: 3.68, workoutType: "walk"},
-    { date: "11/22/2022", workoutQuantity: 6.16, workoutType: "walk"},
-    { date: "11/23/2022", workoutQuantity: 3.34, workoutType: "walk"},
-    { date: "11/24/2022", workoutQuantity: 3.28, workoutType: "walk"},
-    { date: "11/25/2022", workoutQuantity: 3.13, workoutType: "walk"},
-    { date: "11/26/2022", workoutQuantity: 3.83, workoutType: "walk"},
-    { date: "11/27/2022", workoutQuantity: 3.47, workoutType: "walk"},
-    
-    // PUSH-UPS
-    { date: "11/12/2022", workoutQuantity: 100, workoutType: "push"},
-    { date: "11/15/2022", workoutQuantity: 100, workoutType: "push"},
-    { date: "11/19/2022", workoutQuantity: 175, workoutType: "push"},
-    { date: "11/21/2022", workoutQuantity: 145, workoutType: "push"},
-    { date: "11/26/2022", workoutQuantity: 235, workoutType: "push"},
+  // WALKING, RUNNING, SWIMMING
+  { date: "11/12/2022", workoutQuantity: 3.55, workoutType: "walk" },
+  { date: "11/13/2022", workoutQuantity: 4.17, workoutType: "walk" },
+  { date: "11/14/2022", workoutQuantity: 4.09, workoutType: "walk" },
+  { date: "11/15/2022", workoutQuantity: 2.96, workoutType: "walk" },
+  { date: "11/16/2022", workoutQuantity: 1.88, workoutType: "walk" },
+  { date: "11/17/2022", workoutQuantity: 3.61, workoutType: "walk" },
+  { date: "11/18/2022", workoutQuantity: 3.29, workoutType: "walk" },
+  { date: "11/19/2022", workoutQuantity: 4.29, workoutType: "walk" },
+  { date: "11/20/2022", workoutQuantity: 3.45, workoutType: "walk" },
+  { date: "11/21/2022", workoutQuantity: 3.68, workoutType: "walk" },
+  { date: "11/22/2022", workoutQuantity: 6.16, workoutType: "walk" },
+  { date: "11/23/2022", workoutQuantity: 3.34, workoutType: "walk" },
+  { date: "11/24/2022", workoutQuantity: 3.28, workoutType: "walk" },
+  { date: "11/25/2022", workoutQuantity: 3.13, workoutType: "walk" },
+  { date: "11/26/2022", workoutQuantity: 3.83, workoutType: "walk" },
+  { date: "11/27/2022", workoutQuantity: 3.47, workoutType: "walk" },
+  { date: "11/28/2022", workoutQuantity: 2.04, workoutType: "walk" },
+  { date: "11/29/2022", workoutQuantity: 0.74, workoutType: "walk" },
+  { date: "11/30/2022", workoutQuantity: 1.14, workoutType: "walk" },
 
-    // PULL-UPS
-    { date: "11/13/2022", workoutQuantity: 66, workoutType: "pull"},
-    { date: "11/16/2022", workoutQuantity: 20, workoutType: "pull"},
-    { date: "11/20/2022", workoutQuantity: 30, workoutType: "pull"},
-    { date: "11/28/2022", workoutQuantity: 50, workoutType: "pull"},
+  // PUSH-UPS
+  { date: "11/12/2022", workoutQuantity: 100, workoutType: "push" },
+  { date: "11/15/2022", workoutQuantity: 100, workoutType: "push" },
+  { date: "11/19/2022", workoutQuantity: 175, workoutType: "push" },
+  { date: "11/21/2022", workoutQuantity: 145, workoutType: "push" },
+  { date: "11/26/2022", workoutQuantity: 235, workoutType: "push" },
 
-    // FIGHTING
-    { date: "11/19/2022", workoutQuantity: 3, workoutType: "fight"},
-    { date: "11/21/2022", workoutQuantity: 2, workoutType: "fight"},
-    { date: "11/22/2022", workoutQuantity: 2, workoutType: "fight"},
+  // PULL-UPS
+  { date: "11/13/2022", workoutQuantity: 66, workoutType: "pull" },
+  { date: "11/16/2022", workoutQuantity: 20, workoutType: "pull" },
+  { date: "11/20/2022", workoutQuantity: 30, workoutType: "pull" },
+  { date: "11/28/2022", workoutQuantity: 50, workoutType: "pull" },
+
+  // FIGHTING
+  { date: "11/19/2022", workoutQuantity: 3, workoutType: "fight" },
+  { date: "11/21/2022", workoutQuantity: 2, workoutType: "fight" },
+  { date: "11/22/2022", workoutQuantity: 2, workoutType: "fight" },
 ];
